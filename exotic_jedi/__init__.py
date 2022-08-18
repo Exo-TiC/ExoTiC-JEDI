0ebc6fc1a1c4b5e057916fde93c29f167550ff45
Lili Alderson <lili.alderson13@gmail.com>
2022-08-18 14:01:19 +0100

    Updated __init__.py
    Updated init to include all relevant function

--- exotic_jedi/__init__.py
+++ exotic_jedi/__init__.py
@@ -1,7 +1,34 @@
 __all__ = [
     'dq_flat_replace',
+    'construct_spatial_profile',
+    'outliers_through_space',
+    'outliers_through_time',
+    'gauss',
+    'identify_trace',
+    'make_poly',
+    'get_aperture',
+    'f_noise_zone',
+    'remove_fnoise',
+    'make_1f_stack',
+    'ints_to_timeseries',
+    'check_1f',
+    'basic_extraction',
+    'intrapixel_extraction',
+    'correlator',
+    'shift_shift',
+    'get_stellar_spectra',
+    'compare_2d_spectra',
+    'unsegment',
+    'dq_flag_metrics',
     'noise_calculator'
 ]
 
-from .base_functions import dq_flat_replace
-from .base_functions import noise_calculator
+from .base_functions import dq_flat_replace, construct_spatial_profile, outliers_through_space, outliers_through_time, \
+    gauss, identify_trace, make_poly, get_aperture, \
+    f_noise_zone, remove_fnoise, make_1f_stack, ints_to_timeseries, check_1f, \
+    basic_extraction, intrapixel_extraction, \
+    correlator, shift_shift, \
+    get_stellar_spectra, \
+    compare_2d_spectra
+
+from .extra_functions import unsegment, dq_flag_metrics, noise_calculator
